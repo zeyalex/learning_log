56f3da4f2249edb0f2f989ef0662706cc8361147
Olexandr <qwerty@gmail.com>
2024-01-21 11:05:12 +0200

    видалив крапки з string

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Topic(models.Model):
-    """Тема, яку вивчає користувач."""
+    """Тема, яку вивчає користувач"""
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        """Повернути рядкове представлення моделі."""
+        """Повернути рядкове представлення моделі"""
         return self.text
 
 class Entry(models.Model):
