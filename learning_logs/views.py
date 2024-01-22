179ae7e136e92e12e5786a212b53c3b23250d0de
Olexandr <qwerty@gmail.com>
2024-01-22 21:24:47 +0200

    створення головної сторінки

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """Головна сторінка "Журналу спостережень"."""
+    return render(request, 'learning_logs/index.html') 
