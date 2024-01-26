391ac4884055fe9e209738a4224540a56601275a
Olexandr <qwerty@gmail.com>
2024-01-26 10:14:27 +0200

    Додавання шаблонів

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,4 +1,4 @@
-"""Defines URL patterns for learning_logs"""
+"""Визначає регулярні вирази URL для learning_logs"""
 from django.urls import path
 
 from . import views
@@ -7,4 +7,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # головна сторінка
     path('', views.index, name='index'),
+    # Сторінка що відображає всі теми.
+    path('topics/', views.topics, name='topics'),
+    # Сторінка, присвячена окремій темі.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from .models import Topic
+
 def index(request):
     """Головна сторінка "Журналу спостережень"."""
-    return render(request, 'learning_logs/index.html') 
+    return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """Відображає всі теми."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context) 
+
+def topic(request, topic_id):
+    """Показати одну тему та її всі записи."""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic': topic, 'entries': entries}
+    return render(request, 'learning_logs/topic.html', context)
