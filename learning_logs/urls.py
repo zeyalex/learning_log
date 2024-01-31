5634577c083dc30127ddb3f938807be1411b06c6
Olexandr <qwerty@gmail.com>
2024-01-31 21:26:04 +0200

    Додавання тем, дописів та їх редагування

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -11,4 +11,11 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # Сторінка, присвячена окремій темі.
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    #Сторінка для додавання нової теми
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #Сторінка для додавання нового допису
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Сторінка для редагування допису.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
+
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 def index(request):
     """Головна сторінка "Журналу спостережень"."""
@@ -18,3 +19,59 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Додаваня нової теми."""
+    if request.method != 'POST':
+        # Жодних данних не відправлено; створити порожню форму.
+        form = TopicForm()
+    else:
+        # Відправленний POST; обробити дані.
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # ПОказати порожню або не дійсну форму.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """Додавти новий запис до певної теми"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Жодних данних не надіслано створити порожню форму.
+        form = EntryForm()
+    else:
+        # Отримані дані у POST-запиті; обробити дані.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Показати порожню або не дійсну форму.
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Редагувати існуючий запис."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        # початковий запит, попередньо заповніть форму поточним записом.
+        form = EntryForm(instance=entry)
+    else:
+        #POST дані публікації; обробляти дані
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
+
+
+
+
